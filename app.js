d12efba9d8684205f65c44d2907550b9ffa27283
Anay Modi <anaym@umich.edu>
2023-06-26 21:10:27 -0400

    Updates

---
+++ app.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StockSearch from './stocksearch';
+
+function App() {
+  return (
+    <div className="App">
+      <StockSearch/>
+    </div>
+  );
+}
+
+export default App;
